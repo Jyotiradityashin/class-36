3df894d2b5904a1d5f5f029546af829a75ae412e
Jyotiradityashin <87648668+Jyotiradityashin@users.noreply.github.com>
2021-10-11 17:00:40 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,86 @@
+class form{
+constructor(){}
+display(){var title=createElement("H2")
+
+title.html("RACING GAME ")
+
+title.position(150,50) 
+
+var fire1=createInput("Enter ur name")
+fire1.position(140,120)
+var next=createButton("start")
+
+next.position(200,200)
+
+next.mousePressed(
+
+    function(){
+title.hide()
+next.hide()
+fire1.hide()
+
+
+
+
+
+var massge =createElement("H3")
+massge.html ('hey nice to see u  '+fire1.value())
+
+massge.position(100,100)
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var hypnoticBall, database;
+var position;
+var f1 
+
+function setup(){
+  createCanvas(500,500);
+f1= new form()
+  f1.display()
+  
+}
+
+function draw(){
+  background("white");
+  
+    
+  
+}
+
